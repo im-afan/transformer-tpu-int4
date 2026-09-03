0989d15c280a921606923abd6f33abbf3dbf588f
Andrew Fan <andrewkongfan@gmail.com>
2026-09-03 00:37:22 -0700

    remove legacy cuda folder

--- accel/cuda/bindings.cpp
+++
@@ -1,7 +0,0 @@
-#include <torch/extension.h>
-
-std::tuple<torch::Tensor, torch::Tensor> mha_custom(torch::Tensor Q, torch::Tensor K, torch::Tensor V);
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("mha_custom", torch::wrap_pybind_function(mha_custom), "mha_custom");
-}
